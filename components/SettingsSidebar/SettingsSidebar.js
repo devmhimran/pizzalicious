5d5213f13fca470f791552956112b5823f39bfb1
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-06-29 03:45:27 +0600

    [Update] Active Setting Sidebar Nav

--- components/SettingsSidebar/SettingsSidebar.js
+++ components/SettingsSidebar/SettingsSidebar.js
@@ -4,15 +4,13 @@ import { PiMapPinLight } from "react-icons/pi";
 import { BsShield } from "react-icons/bs";
 import styles from "./SettingSidebar.module.css";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 const SettingsSidebar = ({ pathname }) => {
-  const router = useRouter();
   const dashboardNavLink = [
     {
       navText: "Account",
       navSubText: "Personal information",
-      navLink: "/settings/",
+      navLink: "/settings",
       icon: AiOutlineUser,
     },
     {
@@ -34,34 +32,39 @@ const SettingsSidebar = ({ pathname }) => {
       icon: BsShield,
     },
   ];
+
+  const nonActiveNav = "hover:bg-[#fff3f3] hover:border-brandColor01";
+
   return (
     <div className={`container mx-auto `}>
       <div>
         <h2 className="text-xl font-semibold">Settings</h2>
-
         {dashboardNavLink.map((data, index) => (
           <>
-            {/* <Link key={index} href={data.navLink}> */}
-            <div
-              key={index}
-              onClick={() => router.push(data.navLink)}
-              className={`flex items-center gap-3.5 mt-5 p-4 
-            border border-1 rounded-xl my-2 hover:bg-[#fff3f3] 
-            hover:border-brandColor01 ${styles.dashboard__nav}`}
-            >
-              <span
-                className={`bg-[#EDEEF2] p-2 rounded-xl ${styles.dashboard__nav__container}`}
+            <Link key={index} href={data.navLink}>
+              <div
+                key={index}
+                className={`flex items-center gap-3.5 mt-5 p-4 cursor-pointer
+                border border-1 rounded-xl my-2 ${
+                  pathname === data.navLink
+                    ? styles.dashboard__active__nav +
+                      " border-brandColor01 bg-[#fff3f3]"
+                    : nonActiveNav
+                } ${styles.dashboard__nav}`}
               >
-                <data.icon
-                  className={`text-3xl text-[#83859C] ${styles.dashboard__nav__icon}`}
-                />
-              </span>
-              <div>
-                <p className="text-lg font-bold">{data.navText}</p>
-                <p>{data.navSubText}</p>
+                <span
+                  className={`bg-[#EDEEF2] p-2 rounded-xl ${styles.dashboard__nav__container}`}
+                >
+                  <data.icon
+                    className={`text-3xl text-[#83859C] ${styles.dashboard__nav__icon}`}
+                  />
+                </span>
+                <div>
+                  <p className="text-lg font-bold">{data.navText}</p>
+                  <p>{data.navSubText}</p>
+                </div>
               </div>
-            </div>
-            {/* </Link> */}
+            </Link>
           </>
         ))}
       </div>
