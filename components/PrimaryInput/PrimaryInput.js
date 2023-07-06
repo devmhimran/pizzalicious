c30ff9a9b23c7beeb2a9ec982bc2533c0f66bed0
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-07-07 02:35:13 +0600

    [Update] Primary Input

--- components/PrimaryInput/PrimaryInput.js
+++ components/PrimaryInput/PrimaryInput.js
@@ -1,4 +1,8 @@
-"use client";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+const Input = dynamic(() => import("./Input"), { ssr: false });
+
 const PrimaryInput = ({
   className = "",
   type,
@@ -11,27 +15,50 @@ const PrimaryInput = ({
   inputId,
   defaultValue,
 }) => {
-  return (
-    <div className="w-full lg:py-0 py-1">
-      <label className="text-base font-semibold" for={inputId}>
-        {label}
-      </label>
-      <input
-        className={
-          `border rounded-md p-3 w-full outline-none focus:border-brandColor01 mt-1` +
-          className
-        }
-        id={inputId}
-        type={type}
-        name={name}
-        value={value}
-        onBlur={onBlur}
-        onChange={onChange}
-        placeholder={placeholder}
-        defaultValue={defaultValue}
-      />
-    </div>
-  );
+  const [inputValue, setInputValue] = useState(defaultValue);
+
+  useEffect(() => {
+    setInputValue(defaultValue);
+  }, [defaultValue]);
+
+  const handleBlur = (event) => {
+    setInputValue(event.target.value);
+    if (onBlur) {
+      onBlur(event);
+    }
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+    if (onChange) {
+      onChange(event);
+    }
+  };
+
+  // Render the component only on the client-side
+  if (typeof window !== "undefined") {
+    return (
+      <div className="w-full lg:py-0 py-1">
+        <label className="text-base font-semibold" htmlFor={inputId}>
+          {label}
+        </label>
+        <Input
+          className={`border rounded-md p-3 w-full outline-none focus:border-brandColor01 mt-1 ${className}`}
+          id={inputId}
+          type={type}
+          name={name}
+          value={inputValue}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          placeholder={placeholder}
+          defaultValue={defaultValue}
+        />
+      </div>
+    );
+  }
+
+  // Return null during server-side rendering
+  return null;
 };
 
 export default PrimaryInput;
