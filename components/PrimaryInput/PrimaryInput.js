b5f3a780d573db6d6c8d51fc564307988006d25c
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-07-07 02:40:24 +0600

    [Update] Primary Input

--- components/PrimaryInput/PrimaryInput.js
+++ components/PrimaryInput/PrimaryInput.js
@@ -1,8 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-
-const Input = dynamic(() => import("./Input"), { ssr: false });
 
 const PrimaryInput = ({
   className = "",
@@ -36,30 +33,24 @@ const PrimaryInput = ({
     }
   };
 
-  // Render the component only on the client-side
-  if (typeof window !== "undefined") {
-    return (
-      <div className="w-full lg:py-0 py-1">
-        <label className="text-base font-semibold" htmlFor={inputId}>
-          {label}
-        </label>
-        <Input
-          className={`border rounded-md p-3 w-full outline-none focus:border-brandColor01 mt-1 ${className}`}
-          id={inputId}
-          type={type}
-          name={name}
-          value={inputValue}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          placeholder={placeholder}
-          defaultValue={defaultValue}
-        />
-      </div>
-    );
-  }
-
-  // Return null during server-side rendering
-  return null;
+  return (
+    <div className="w-full lg:py-0 py-1">
+      <label className="text-base font-semibold" htmlFor={inputId}>
+        {label}
+      </label>
+      <input
+        className={`border rounded-md p-3 w-full outline-none focus:border-brandColor01 mt-1 ${className}`}
+        id={inputId}
+        type={type}
+        name={name}
+        value={inputValue}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+      />
+    </div>
+  );
 };
 
 export default PrimaryInput;
